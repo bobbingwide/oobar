eea1a0da6f062257d4ae7fee5268ebfc714c613c
Herb Miller <herb@bobbingwide.com>
2016-04-04 10:49:54 +0100

    Added a note about checking status of this TRAC

--- trac28727-before-edit.php
+++ trac28727-before-edit.php
@@ -9,5 +9,5 @@
 function trac2827() {
   echo "The character appearing as lower case u-acute started life as a pound sterling character ";
   echo "Surely its presence should not cause the plugin editor to display absolutely nothing for this source file";
-  
+  echo "2016/02/26 - should check if this has been fixed one day";
 } 
